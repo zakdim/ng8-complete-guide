91e863e3b9f5ed64b5b72836b6f374daad291dd6
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2019-07-06 11:45:34 -0400

    20 - working with comp styles

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  // templateUrl: './servers.component.html',
   template: `
     <app-server></app-server>
     <app-server></app-server>
