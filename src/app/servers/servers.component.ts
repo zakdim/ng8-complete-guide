1a7d0a595d369ffcfebf847da24475a0792b29eb
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2019-07-06 11:56:09 -0400

    s02-21 - comp selector

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  // templateUrl: './servers.component.html',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>
-  `,
+  selector: 'app-servers', // select by element
+  // selector: '[app-servers]', // by attrubute
+  // selector: '.app-servers', // by class
+  templateUrl: './servers.component.html',
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  // `,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
