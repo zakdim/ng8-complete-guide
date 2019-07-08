4c3579b5afbe16fc6b5e21cc20fac985a2502885
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2019-07-07 21:13:39 -0400

    s02-29 - passing data with event binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -14,6 +14,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
+  serverName = '';
 
   constructor() {
     setTimeout(() => {
@@ -24,12 +25,15 @@ export class ServersComponent implements OnInit {
   ngOnInit() {
   }
 
-  onCreateServer(event) {
-    console.log(`Button: ${event.target}`);
+  onCreateServer() {
     this.serverCreationStatus = 'Server was created!';
   }
 
   onButtonBlur(event: any) {
     console.log(`Button '${event.target.innerText}' blur event occurred`);
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
